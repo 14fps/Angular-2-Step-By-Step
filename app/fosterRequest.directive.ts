c924fdf69a0e77091849192c0a9a8f4284601c7b
Luke Angel <contact@btreepress.com>
2017-02-10 23:18:57 -0800

    4- init

--- app/fosterRequest.directive.ts
+++ app/fosterRequest.directive.ts
@@ -1,10 +1,20 @@
-import {Directive, HostBinding,Input} from '@angular/core';
+import {Directive, HostBinding,HostListener, Input} from '@angular/core';
  
 @Directive({
 	selector:'[tprFosterRequest]'
 })
 export class FosterRequestDirective{
-   @HostBinding('class.is-fosterRequest') fosterRequest = true;
+  @HostBinding('class.is-fosterRequest') fosterRequest = true;
+  @HostBinding('class.is-fosterRequest-hovering') hovering = false;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+  }
+
    @Input() set tprFosterRequest(value) {
     this.fosterRequest = value;
   }
