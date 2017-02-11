bed8cce93666d4a616ac491aa63ece84a9b768f2
Luke Angel <contact@btreepress.com>
2017-02-10 23:14:52 -0800

    4-2 init

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {PetComponent} from './pet.component';
+import {PetListComponent} from './pet-list.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,9 @@ import {PetComponent} from './pet.component';
   ],
   declarations: [
     AppComponent,
-    PetComponent
+    PetComponent,
+    PetListComponent
+    
   ],
   bootstrap: [
     AppComponent
