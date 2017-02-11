bcdbb861fa7048ba944e60718ff032ebb7cb3a71
Luke Angel <contact@btreepress.com>
2017-02-10 23:18:06 -0800

    4-5 init

--- app/fosterRequest.directive.ts
+++ app/fosterRequest.directive.ts
@@ -1,9 +1,11 @@
-import {Directive, HostBinding} from '@angular/core';
-
+import {Directive, HostBinding,Input} from '@angular/core';
+ 
 @Directive({
 	selector:'[tprFosterRequest]'
 })
 export class FosterRequestDirective{
    @HostBinding('class.is-fosterRequest') fosterRequest = true;
-
+   @Input() set tprFosterRequest(value) {
+    this.fosterRequest = value;
+  }
 }
