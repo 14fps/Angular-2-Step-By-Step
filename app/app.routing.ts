9777578407be7b0eebae8785fc6a4952a9397a1f
Luke Angel <contact@btreepress.com>
2017-02-10 23:37:58 -0800

    8-1

---
+++ app/app.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+import { PetFormComponent } from './pet-form.component'
+import { PetListComponent } from './pet-list.component'
+
+const appRoutes: Routes = [
+  { path: 'add' , component: PetFormComponent } ,
+  { path:':type', component: PetListComponent},
+  { path:'', pathMatch:'full', redirectTo: 'all'},
+  
+];
